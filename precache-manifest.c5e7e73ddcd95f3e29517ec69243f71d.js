15cb3fda6acf88ddace68e4d2fae7273f3c27349
Ashris Choudhury <ashris.me@gmail.com>
2020-12-11 03:52:17 +0530

    Add files

---
+++ precache-manifest.c5e7e73ddcd95f3e29517ec69243f71d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0de64194873705ecf0adde204052b7",
+    "url": "/agamiscape/index.html"
+  },
+  {
+    "revision": "6491cf60fec38a648a72",
+    "url": "/agamiscape/static/css/main.098ac163.chunk.css"
+  },
+  {
+    "revision": "cdfb03de1ebca251e972",
+    "url": "/agamiscape/static/js/2.0824672f.chunk.js"
+  },
+  {
+    "revision": "6491cf60fec38a648a72",
+    "url": "/agamiscape/static/js/main.2b2ddf9e.chunk.js"
+  },
+  {
+    "revision": "bd49ee7ba9a010a1f494",
+    "url": "/agamiscape/static/js/runtime~main.93f0b5ef.js"
+  },
+  {
+    "revision": "4109f7217d6e4516aba56b3293ca8856",
+    "url": "/agamiscape/static/media/agami-prize.4109f721.png"
+  },
+  {
+    "revision": "7cb9704b827b1706ccaf0f01bcaf1e70",
+    "url": "/agamiscape/static/media/legend.7cb9704b.png"
+  },
+  {
+    "revision": "b50f353aa36af26c9c415a04e5408c8d",
+    "url": "/agamiscape/static/media/loader.b50f353a.gif"
+  }
+]);
